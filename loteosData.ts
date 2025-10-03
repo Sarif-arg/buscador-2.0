f089fe5c1262e758fb451e2961a463d5916ed961
Federico Mas <132709197+Sarif-arg@users.noreply.github.com>
2025-10-03 14:47:27 -0300

    Update loteosData.ts

--- loteosData.ts
+++ loteosData.ts
@@ -3,27 +3,27 @@ import type { Loteo } from './types';
 
 export const LOTEOS_DATA: Loteo[] = [
   {
-    id: 'tierra-de-suenos',
-    name: 'Tierra de Sueños',
-    imageUrl: 'https://picsum.photos/seed/loteo1/400/400',
-    url: 'https://almironpropiedades.com.ar/loteos/tierra-de-suenos'
+    id: 'terranova',
+    name: 'Terranova',
+    imageUrl: 'https://d1v2p1s05qqabi.cloudfront.net/16509788/conversions/1752260857-thumbnail.webp',
+    url: 'https://almironpropiedades.com.ar/terranova-el-lugar-ideal-para-construir-tu-futuro'
   },
   {
-    id: 'puerto-roldan',
-    name: 'Puerto Roldán',
-    imageUrl: 'https://picsum.photos/seed/loteo2/400/400',
-    url: 'https://almironpropiedades.com.ar/loteos/puerto-roldan'
+    id: 'portofino',
+    name: 'Portofino',
+    imageUrl: 'https://d1v2p1s05qqabi.cloudfront.net/sites/152/media/1743440203526.webp',
+    url: 'https://almironpropiedades.com.ar/portofino-el-lugar-donde-los-suenos-se-convierten-en-hogar'
   },
   {
-    id: 'don-carlos',
-    name: 'Don Carlos',
-    imageUrl: 'https://picsum.photos/seed/loteo3/400/400',
-    url: 'https://almironpropiedades.com.ar/loteos/don-carlos'
+    id: 'fincas-del-rosedal',
+    name: 'Fincas del Rosedal',
+    imageUrl: 'https://d1v2p1s05qqabi.cloudfront.net/sites/152/media/1729535975565.webp',
+    url: 'almironpropiedades.com.ar/fincas-del-rosedal-etapa-2-vida-de-pueblo-a-solo-5-minutos-de-la-ciudad'
   },
   {
-    id: 'brisas-funes',
-    name: 'Brisas de Funes',
-    imageUrl: 'https://picsum.photos/seed/loteo4/400/400',
-    url: 'https://almironpropiedades.com.ar/loteos/brisas-funes'
+    id: 'fincas-ros',
+    name: 'Fincas Ros',
+    imageUrl: 'https://d1v2p1s05qqabi.cloudfront.net/sites/152/media/1733154009175.webp',
+    url: 'https://almironpropiedades.com.ar/fincas-ros-cerca-de-todo'
   },
 ];
